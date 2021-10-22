14c1aeb70e5587c78a93f197833d9921d7016ae7
Terry Ferreira <13345216+TTFerreira@users.noreply.github.com>
2021-10-22 14:57:47 +1300

    CC-9 Created model, factory, migration, seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             OrganisationSeeder::class,
             CustomerSeeder::class,
+            ProductTypeSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
